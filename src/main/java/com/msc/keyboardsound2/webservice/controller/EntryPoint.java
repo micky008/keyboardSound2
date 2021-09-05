5fd83d42b70596c001718397347cb51cb8ab0213
mchinchole <michael.chinchole@safety-line.fr>
2021-09-06 00:27:30 +0200

    correction refresh

--- src/main/java/com/msc/keyboardsound2/webservice/controller/EntryPoint.java
+++ src/main/java/com/msc/keyboardsound2/webservice/controller/EntryPoint.java
@@ -1,6 +1,8 @@
 package com.msc.keyboardsound2.webservice.controller;
 
+import com.msc.keyboardsound2.server.Config;
 import com.msc.keyboardsound2.server.SoundList;
+import java.io.IOException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -17,8 +19,9 @@ public class EntryPoint {
     @GET
     @Path("refresh")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response refresh() {
-        SoundList.refreshStatic();
+    public Response refresh() throws IOException {
+        Config config = Config.getConfig("config.json");
+        new SoundList(config); //init la liste static 
 	return Response.ok().build();
     }
        
