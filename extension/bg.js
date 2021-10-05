92edcdbc80b14363c32c2e7c1bc2f5d4bee6f6c6
mchinchole <michael.chinchole@safety-line.fr>
2021-10-05 22:34:54 +0200

    new bg

--- extension/bg.js
+++ extension/bg.js
@@ -1,15 +1,18 @@
 var readsound;
 var audioElement = new Audio();
 
+setInterval(reconnect,1000 * 60 * 5);
 reconnect();
 
 function reconnect(){
+	if (readsound != undefined) {
+		readsound.close();
+	}
 	readsound = new WebSocket("ws://micky.ovh:9998/writesound");
 	readsound.onmessage = readsoundOnMessage;
 }
 
 function readsoundOnMessage(event) {
-	let audioElement = new Audio();
     audioElement.src = 'data:audio/mp3;base64, ' +event.data;
     audioElement.load();
     audioElement.play();
